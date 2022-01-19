fe5617b4814228abbc5d1ee97a7d6ab70bb3943d
Anhbman <anhbumbk@gmail.com>
2022-01-20 00:16:21 +0700

    teacer auth

--- app/Http/Middleware/AdminAccess.php
+++ app/Http/Middleware/AdminAccess.php
@@ -19,15 +19,15 @@ class AdminAccess
         $user = Auth::user();
         return $next($request);
         // echo $user;
-//        if($user)
-//        {
-//            if($user->User_role == 0)
-//            {
-//                // $request->attributes->add(['ID'=>$user->User_ID]);
-//                return $next($request);
-//            }
-//            return response()->json(['message'=>'Dang nhap bang admin di'],403);
-//        }
-//        else return response()->json(['message'=>'Dang nhap bang admin di'],403);
+        if($user)
+        {
+            if($user->User_role == 0)
+            {
+                // $request->attributes->add(['ID'=>$user->User_ID]);
+                return $next($request);
+            }
+            return response()->json(['message'=>'Dang nhap bang admin di'],403);
+        }
+        else return response()->json(['message'=>'Dang nhap bang admin di'],403);
     }
 }
